b8809353a7678d5bcff2937310b69b2d8ebd80c8
jmz <aiyoudqrjmz@163.com>
2020-02-23 15:12:16 +0800

    注释

--- check-version.js
+++ check-version.js
@@ -5,8 +5,10 @@ const request = require('request')
 
 const registry = 'https://registry.npmjs.org/@dcloudio/'
 
+// 获取packages目录下的所有目录
 const pkgs = fs.readdirSync(path.resolve(__dirname, 'packages'))
 
+// nodejs进程运行时，获取命令行的参数 node index.js a b
 const tag = process.argv[2] || 'alpha'
 
 pkgs.forEach(pkg => {
