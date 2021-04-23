9ecb5af8937ae69f05698c56932d55ac23feafc1
Boswell <mingzhuang.ji@ly.com>
2021-04-23 19:45:19 +0800

    test

--- src/core/vue.js
+++ src/core/vue.js
@@ -30,22 +30,16 @@ export default function initVue (Vue) {
     }
   }
 
-  // 装饰器
+
   const oldIsReservedTag = Vue.config.isReservedTag
-  // 被保留的标签
   Vue.config.isReservedTag = function (tag) {
-    // 使用的标签是否是保留标签
     return tags.indexOf(tag) !== -1 || oldIsReservedTag(tag)
   }
 
-  // 被忽略元素
   Vue.config.ignoredElements = tags
-  // 获取Vue配置标签命名空间
   const oldGetTagNamespace = Vue.config.getTagNamespace
-  // uniapp与html冲突标签
   const conflictTags = ['switch', 'image', 'text', 'view']
 
-  // 获取标签的名称空间
   Vue.config.getTagNamespace = function (tag) {
     // ~ -1 为 0, 冲突标签返回false,没有名称空间
     if (~conflictTags.indexOf(tag)) { // svg 部分标签名称与 uni 标签冲突
